7d5870cb61e9edf43334a4d4f04c5c07f166d44c
Specialwu-cpu <1307053712@qq.com>
2023-03-12 14:24:29 +0800

    创建MovieHub-server项目

---
+++ src/main/java/com/moviehub/server/MovieHubServerApplication.java
@@ -0,0 +1,13 @@
+package com.moviehub.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieHubServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieHubServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moviehub/server/MovieHubServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviehub.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieHubServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
